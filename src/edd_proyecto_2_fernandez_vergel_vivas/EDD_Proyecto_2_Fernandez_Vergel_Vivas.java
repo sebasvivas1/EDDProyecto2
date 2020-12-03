97e3b40a759cb274727d37aee327cd173243abb9
Sebastian Vivas Windows <sebasvivas107@gmail.com>
2020-12-03 11:28:29 -0400

    Initial commit & gitignore

---
+++ src/edd_proyecto_2_fernandez_vergel_vivas/EDD_Proyecto_2_Fernandez_Vergel_Vivas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd_proyecto_2_fernandez_vergel_vivas;
+
+/**
+ *
+ * @author tatan
+ */
+public class EDD_Proyecto_2_Fernandez_Vergel_Vivas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
